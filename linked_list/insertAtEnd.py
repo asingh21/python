d040edca75841b167d87890365d2717a5ad0f53c
Ankush Singh <asingh21@ncsu.edu>
2018-04-09 21:44:55 -0700

    add at the end of the linked list

---
+++ linked_list/insertAtEnd.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next = next_node
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def add_node(self, data):
+        new_node = Node(data, self.head)
+        self.head = new_node
+
+    def insert_at_the_end(self, data):
+        temp = self.head
+        while(temp.next != None):
+            temp = temp.next
+        new_node = Node(data)
+        temp.next = new_node
+
+    def print_linked_list(self):
+        temp = self.head
+        while(temp):
+            print temp.data
+            temp = temp.next
+
+llist = LinkedList()
+llist.add_node(5)
+llist.add_node(10)
+llist.add_node(15)
+llist.insert_at_the_end(20)
+llist.print_linked_list()
