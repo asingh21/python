f5536ae026d661058dad81c475b672b85efce22f
Ankush Kumar Singh <asingh21@ncsu.edu>
2018-10-22 22:28:02 -0700

    updating stacks uing dymanic list code

--- stacks/stacks_using_dynamic_list.py
+++ stacks/stacks_using_dynamic_list.py
@@ -1,5 +1,5 @@
 class Stack(object):
-    def __init__(self, limit=10):
+    def __init__(self, limit=5):
         self.stack = []
         self.limit = limit
 
@@ -7,7 +7,7 @@ class Stack(object):
         return len(self.stack) == 0
 
     def push(self, data):
-        if len(self.stack >= self.limit):
+        if len(self.stack) >= self.limit:
             print ("Stack overflow! Resizing")
             self.resize()
         self.stack.append(data)
@@ -35,4 +35,12 @@ class Stack(object):
         self.limit = 2 * self.limit
         self.stack = new_stack
 
-data
+data_stack = Stack()
+data_stack.push("1")
+data_stack.push("2")
+data_stack.push("3")
+data_stack.push("4")
+data_stack.push("5")
+data_stack.push("6")
+print data_stack.peek()
+print data_stack.pop()
