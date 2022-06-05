75caa617d1f74fc3f27f19a3f11dfb709a3d2ae8
Ankush Singh <asingh21@ncsu.edu>
2022-06-05 12:02:27 -0700

    Deleting duplicate files

--- arrays/RotateUsingGCD.py
+++
@@ -1,9 +0,0 @@
-def get_gcd(num1, num2):
-    while num1 % num2 != 0:
-        temp = num1
-        num1 = num2
-        num2 = temp % num2
-        print "num1 = {} and nume = {}".format(num1, num2)
-    print "gcd = {}".format(num2)
-
-get_gcd(45,10)
